82d160683923008e230184c1a2fc1fe16e8138dd
sangrov1 <sangrov1@in.ibm.com>
2019-07-25 23:19:41 +0530

    Removed all hard coded data from the application code to application.properties

--- src/main/java/com/example/muzixapp/seeddata/ApplicationListenerDemo.java
+++ src/main/java/com/example/muzixapp/seeddata/ApplicationListenerDemo.java
@@ -3,18 +3,28 @@ package com.example.muzixapp.seeddata;
 import com.example.muzixapp.domain.Track;
 import com.example.muzixapp.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("application.properties")
 public class ApplicationListenerDemo implements ApplicationListener<ApplicationReadyEvent> {
 
     @Autowired
     private TrackRepository trackRepository;
 
+    @Value("${id1}")
+    int id1;
+    @Value("${name1}")
+    String name1;
+    @Value("${comment1}")
+    String comment1;
+
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
-        trackRepository.save(new Track(1, "Suit Punjabi", "Siraa!"));
+        trackRepository.save(new Track(id1, name1, comment1));
     }
 }
